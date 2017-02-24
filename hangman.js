21c80371c823688d0ade72a84efc58be64b7a0ca
Matt Carlson <mcarlson8198@gmail.com>
2017-02-23 17:07:57 -0800

    version 1 of hangman game with images and gradient background

---
+++ hangman.js
@@ -0,0 +1,21 @@
+
+
+/*
+click button to play
+prompt for word
+determine if any non letters in array
+if yes => request new word (letters only)
+if no => store word as array
+
+input field to guess letter
+click event triggers:
+- take in value
+- determine if it is a letter (not a number or other char)
+- if no => request different character
+- if yes
+  - loop through array and find the first match
+  - if match => display "Nice!"
+  - if no match => decrement counter, hide "Nice!"
+
+
+*/
